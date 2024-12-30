7a8f2ecd62388289891473fdbb0ec8e2115335a0
sqrtyraccoon <uwuplink@gmail.com>
2024-12-30 15:51:11 +0100

    First setup

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+print('Haiii !')
+
+
+
+
+
+while True:
+    cmd = input('> ')
+    if cmd == 'exit':
+        print('Bye :<')
+        break
+    if cmd == 'clear':
+        os.system('clear')
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+clear
+
+python '/home/sqrtyracc/Documents/Terminal_RPG/main.py'
